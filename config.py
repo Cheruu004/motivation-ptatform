b67d774c37f303a364d54fd68c914c5ef2c39994
Cheruu004 <briankoech010@gmail.com>
2024-08-19 08:08:30 -0400

    configure render db

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://moringa_user:Brian_6534@localhost/moringa_platform_db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://moringa_platform_db_user:dEnyThz25UOcyxJpIEhv93E43KyNprBR@dpg-cr1d5frtq21c73crjt4g-a.oregon-postgres.render.com/moringa_platform_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret'
     
